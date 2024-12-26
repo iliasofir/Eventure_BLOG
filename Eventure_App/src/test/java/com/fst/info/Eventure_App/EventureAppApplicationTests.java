ba31a1e0a370c39bb06b9eaf941ef752825721dd
admin <admin@localhost>
2024-12-26 16:27:26 +0100

    initial commit (good luck)

---
+++ Eventure_App/src/test/java/com/fst/info/Eventure_App/EventureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.info.Eventure_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
